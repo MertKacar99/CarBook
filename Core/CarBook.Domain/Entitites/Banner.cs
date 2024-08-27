5937848335d2f7cebbf77b4fafab64b6f09b28dc
Mert <mertkacar5800@gmail.com>
2024-08-27 22:16:37 +0300

    Proje dosyası ekle.

---
+++ Core/CarBook.Domain/Entitites/Banner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Core
+{
+    public class Banner
+    {
+        public int BannerId { get; set; }
+        public  string title { get; set; }
+        public string Description { get; set; }
+        public string VideDescription { get; set; }
+        public string VideoUrl { get; set; }
+
+
+    }
+}
