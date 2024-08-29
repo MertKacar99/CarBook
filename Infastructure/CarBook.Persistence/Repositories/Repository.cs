b33b3322446c6c8e6ff17cb4246f0e39823ff318
Mert <mertkacar5800@gmail.com>
2024-08-29 16:56:23 +0300

    Added Repository Class

---
+++ Infastructure/CarBook.Persistence/Repositories/Repository.cs
@@ -0,0 +1,40 @@
+﻿using CarBook.Application.Interfaces;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        
+
+        Task IRepository<T>.CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IRepository<T>.DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<T>> IRepository<T>.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<T> IRepository<T>.GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IRepository<T>.UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
