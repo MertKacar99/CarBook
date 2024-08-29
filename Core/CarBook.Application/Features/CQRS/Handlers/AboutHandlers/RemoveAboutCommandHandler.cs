79f9f23b10cdb0137a2378ec716d4150232c71bf
Mert <mertkacar5800@gmail.com>
2024-08-29 19:10:54 +0300

    controller and some classes added.

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/RemoveAboutCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CarBook.Application.Features.CQRS.Commands.AboutCommands;
+using CarBook.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Domain.Entitites;
+
+namespace CarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class RemoveAboutCommandHandler
+    {
+        private readonly IRepository<About> _aboutRepository;
+
+        public RemoveAboutCommandHandler(IRepository<About> aboutRepository)
+        {
+            _aboutRepository = aboutRepository;
+        }
+        public async Task Handle(RemoveAboutCommand command)
+        {
+            var value = await _aboutRepository.GetByIdAsync(command.Id);
+            await _aboutRepository.RemoveAsync(value);
+        
+        }
+    }
+}
