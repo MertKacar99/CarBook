3e43f4f7804d74fc4608858f1b9dc5f50a80a2de
Mert <mertkacar5800@gmail.com>
2024-08-29 18:07:50 +0300

    Added GetAboutHandler

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using CarBook.Application.Features.CQRS.Results.AboutResults;
+using CarBook.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Domain.Entitites;
+
+namespace CarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+        
+        public async Task<List<GetAboutByIdQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return
+                   values.Select(x => new GetAboutByIdQueryResult
+                   {
+                       AboutID = x.AboutID,
+                       Description = x.Description,
+                       ImageUrl = x.ImageUrl,
+                       Title=  x.Title,
+
+                   }).ToList();
+        }
+    }
+}
