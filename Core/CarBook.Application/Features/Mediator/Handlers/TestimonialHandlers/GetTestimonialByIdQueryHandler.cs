ebc30b280dd20c10fba72308fc7618af698916ae
Mert <mertkacar5800@gmail.com>
2024-09-06 17:27:24 +0300

    added Testimonial crod class

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/TestimonialHandlers/GetTestimonialByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using CarBook.Application.Features.Mediator.Queries.TestimonialQueries;
+using CarBook.Application.Features.Mediator.Results.TestimonialResults;
+using CarBook.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Domain.Entitites;
+
+namespace CarBook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class GetTestimonialByIdQueryHandler : IRequestHandler<GetTestimonialByIdQuery, GetTestimonialGetByIdQueryResult>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public GetTestimonialByIdQueryHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetTestimonialGetByIdQueryResult> Handle(GetTestimonialByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values =   await  _repository.GetByIdAsync(request.id);
+
+            return new GetTestimonialGetByIdQueryResult
+            {
+                Comment = values.Comment,
+                ImageUrl = values.ImageUrl,
+                Name = values.Name,
+                TestimonialID = request.id,
+                Title = values.Title,
+            };
+        
+        }
+    }
+}
