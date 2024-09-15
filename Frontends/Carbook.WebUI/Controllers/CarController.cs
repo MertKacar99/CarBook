770bc77c7067cb1e517af7ea0374a38b9ba3d78d
Mert <mertkacar5800@gmail.com>
2024-09-15 20:32:20 +0300

    Started making Contact and Footer address dynamic.

---
+++ Frontends/Carbook.WebUI/Controllers/CarController.cs
@@ -0,0 +1,33 @@
+﻿using Carbook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Carbook.WebUI.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5265/api/Cars/GetCarWithBrand");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCArWithBrandsDtos>>(jsonData);
+
+                return View(values ?? new List<ResultCArWithBrandsDtos>());
+            }
+
+            // Eğer API başarısız olursa boş model gönderin
+            return View(new List<ResultCArWithBrandsDtos>());
+        }
+    }
+}
---
+++ Frontends/Carbook.WebUI/ViewComponents/FooterAdressComponents/_FooterAdressComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Dto.FooterAdressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Carbook.WebUI.ViewComponents.FooterAdressComponents
+{
+    public class _FooterAdressComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterAdressComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5265/api/FooterAddress");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAdressDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+
+        }
+    }
+}
