7df6be6564a19324a8bed8ee89266ddf1c278dfd
Mert <mertkacar5800@gmail.com>
2024-09-20 00:34:31 +0300

    blog detail development started

--- Core/CarBook.Application/Features/CQRS/Results/ContactResults/GetContactByIdQueryResult.cs
+++ Core/CarBook.Application/Features/CQRS/Results/ContactResults/GetContactByIdQueryResult.cs
@@ -13,6 +13,8 @@ namespace CarBook.Application.Features.CQRS.Results.ContactResults
         public string Email { get; set; }
         public string Subject { get; set; }
         public string Message { get; set; }
+ 
+
         public DateTime SendDate { get; set; }
     }
 }
--- Frontends/Carbook.Dto/BlogDtos/ResultAllBlogsWithAuthorDto.cs
+++ Frontends/Carbook.Dto/BlogDtos/ResultAllBlogsWithAuthorDto.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UdemyCarBook.Domain.Entitites;
 
-namespace CarBook.Domain.Entitites
+namespace Carbook.Dto.BlogDtos
 {
-    public class Blog
+    public class ResultAllBlogsWithAuthorDto
     {
+
         public int BlogID { get; set; }
         public string Title { get; set; }
         public int AuthorID { get; set; }
-        public Author Author { get; set; }
         public string CoverImageUrl { get; set; }
         public DateTime CreatedDate { get; set; }
         public int CategoryID { get; set; }
-        public Category Category { get; set; }
+
+        // include ettiğimiz veriyi alabilmek için prop olarak author ekliyoruz
+        public string AuthorName { get; set; }
         public string Description { get; set; }
 
-    }
 
+    }
 }
