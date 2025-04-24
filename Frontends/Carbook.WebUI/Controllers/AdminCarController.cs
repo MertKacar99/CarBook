6fd3cc8a1c11364132d0b3358b428cf16d709ef7
Mert <mertkacar5800@gmail.com>
2025-04-24 22:32:11 +0300

    Add admin controllers and layout structure
    Introduces `AdminCarController` and `AdminLayoutController` with `Index` actions. Adds `AdminHeaderPartial` for the header layout. Updates `Index.cshtml` to set the page title, layout, and includes a sidebar and footer, enhancing the admin interface's structure and user experience.

---
+++ Frontends/Carbook.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Carbook.WebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/Carbook.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Carbook.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult AdminHeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}
