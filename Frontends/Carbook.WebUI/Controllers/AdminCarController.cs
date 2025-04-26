0444bb6e97d58b8d449e14d02a87f4cdb03c7846
Mert <mertkacar5800@gmail.com>
2025-04-26 21:44:40 +0300

    Enhance admin interface with new features and layout
    - Updated `AdminCarController.cs` to fetch car data asynchronously from an API and pass it to the view.
    - Modified `AdminLayoutController.cs` to add partial view methods for sidebar, footer, and scripts.
    - Revamped `Index.cshtml` to display a structured list of cars with action buttons.
    - Made minor adjustments to `AdminNavbarPartial.cshtml` for better alignment.
    - Created `AdminFooterPartial.cshtml` for footer content with current year and design credit.
    - Added `AdminScriptPartial.cshtml` for necessary JavaScript files.
    - Introduced `AdminSidebarPartial.cshtml` for improved navigation in the admin panel.

--- Frontends/Carbook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/Carbook.WebUI/Controllers/AdminCarController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Carbook.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private IHttpClientFactory httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
         {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7233/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+               var jsonData = await responseMessage.Content.ReadAsStringAsync();
+               var values= JsonConvert.DeserializeObject<List<ResultCArWithBrandsDtos>>(jsonData);
+               return View(values);
+
+
+            }
             return View();
         }
     }
