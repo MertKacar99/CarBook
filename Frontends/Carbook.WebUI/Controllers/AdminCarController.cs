36ef2556929f98007a3aa99fec395d16b0f8b3e7
Mert <mertkacar5800@gmail.com>
2025-04-26 23:21:46 +0300

    Add car creation functionality and related DTOs
    - Implemented `CreateCar` action in `AdminCarController` to fetch car brands from an API and populate a dropdown in the view.
    - Added `ResultBrandDto` for brand data representation.
    - Introduced `CreateCarDto` for car creation properties.
    - Created `CreateCar.cshtml` view for car entry form with brand selection and other input fields.

--- Frontends/Carbook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/Carbook.WebUI/Controllers/AdminCarController.cs
@@ -1,5 +1,7 @@
-﻿using Carbook.Dto.CarDtos;
+﻿using Carbook.Dto.BrandDtos;
+using Carbook.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace Carbook.WebUI.Controllers
@@ -8,9 +10,9 @@ namespace Carbook.WebUI.Controllers
     {
         private IHttpClientFactory httpClientFactory;
 
-        public AdminCarController(IHttpClientFactory httpClientFactory)
+        public AdminCarController(IHttpClientFactory _httpClientFactory)
         {
-            this.httpClientFactory = httpClientFactory;
+            this.httpClientFactory = _httpClientFactory;
         }
 
         public async Task<IActionResult> Index()
@@ -27,5 +29,22 @@ namespace Carbook.WebUI.Controllers
             }
             return View();
         }
+        [HttpGet]
+        public async Task<IActionResult> CreateCar()
+        {
+            var client = httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7233/api/Brands");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            List<SelectListItem> brandValues =(from x in values
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.name,
+                                                   Value = x.brandID.ToString()
+                                               }).ToList();
+            ViewBag.brandValues = brandValues;
+            return View();
+        }
+
     }
 }
